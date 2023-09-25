f08b725dac008ef971dc8f70f78aa0804623fec4
Mehmet Ali <78815495+mehmetalidag97@users.noreply.github.com>
2023-09-25 09:23:57 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+namespace sa_test {
+    export function plotAt(index: number): 
+    
+void {
+    index |= 0
+    const y = Math.floor(index / 5);
+        const x = Math.floor(index % 5);
+        led.plot(x,y)
+}
+}
